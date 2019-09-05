0c64cc29a13a5fdc1713a1d0433ad92f85fb4110
wolfene2001 <wolfene2001@yahoo.com>
2019-09-05 15:06:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #index = " "
-  if board[index] == "X" || "O"
+  if board[index] == "X" || board[index] =="O"
    true
  else board[index] == " " || "" || nil
      false
